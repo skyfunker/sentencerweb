35a1e9e0e2fbc677ec9a83bc83ba37af4c42446d
Andriy Isayev <andriy.isayev@gmail.com>
2015-07-07 18:18:33 +0300

    Form file imput button added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+jQuery(document).on('change', '.btn-file :file', function() {
+        var input = $(this);
+        var numFiles = input.get(0).files ? input.get(0).files.length : 1;
+        var label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
+});
+    
 jQuery(document).ready(function($) {
 
     /* ======= Scrollspy ======= */
@@ -69,6 +76,16 @@ jQuery(document).ready(function($) {
 		
 	});
 	
-
-
+ $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+        
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+        
+        if( input.length ) {
+            input.val(log);
+        } else {
+            if( log ) alert(log);
+        }
+        
+    });
 });
